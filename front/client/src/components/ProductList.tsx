6e4cd45128b409bf78ef8e74148f60f2c82000c5
Challelign <chalie2123@gmail.com>
2025-10-11 17:03:36 +0300

    View all product link and product page created and Filer by is added to products page

--- front/client/src/components/ProductList.tsx
+++ front/client/src/components/ProductList.tsx
@@ -3,15 +3,15 @@ import Categories from './Categories'
 import { products } from '@/sample/productList'
 import ProductCard from './ProductCard'
 import Link from 'next/link'
-import { ProductType } from '@/types/types'
+import Filter from './Filter'
 
-const ProductList = ({category}:{category:string}) => {
+const ProductList = ({category,params}:{category:string, params:"homepage" | "products"}) => {
 
 
   return (
       <div className="w-full">
         <Categories/>
- 
+        {params==="products" &&  <Filter /> }
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
