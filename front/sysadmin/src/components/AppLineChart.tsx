c12e4dc0840808b0218110fc3e78170389677e5c
Challelign <chalie2123@gmail.com>
2025-10-13 22:27:46 +0300

    admin dashboard complete

--- front/sysadmin/src/components/AppLineChart.tsx
+++ front/sysadmin/src/components/AppLineChart.tsx
@@ -1,12 +1,7 @@
 "use client";
 
 import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
-import {
-  ChartConfig,
-  ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
-} from "./ui/chart";
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "./ui/chart";
 
 const chartData = [
   { month: "January", desktop: 186, mobile: 80 },
@@ -46,7 +41,11 @@ const AppLineChart = () => {
           tickMargin={8}
           tickFormatter={(value) => value.slice(0, 3)}
         />
-        <YAxis tickLine={false} axisLine={false} tickMargin={8} />
+        <YAxis
+          tickLine={false}
+          axisLine={false}
+          tickMargin={8}
+        />
         <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
         <Line
           dataKey="desktop"
